300c964cd258b59a086cc9e58d803eafe899c7a4
Elena Cavallero <elenacavallero@outlook.com>
2021-08-18 16:13:58 +0100

    create customer area

--- assets/application.js
+++ assets/application.js
@@ -6,3 +6,46 @@ if (document.getElementById("sort_by") !== null) {
     window.location = url.href;
   });
 }
+
+if (document.getElementById("AddressCountryNew") != null) {
+  document
+    .getElementById("AddressCountryNew")
+    .addEventListener("change", function (e) {
+      var provinces =
+        this.options[this.selectedIndex].getAttribute("data-provinces");
+      var provinceSelector = document.getElementById("AddressProvinceNew");
+      var provinceArray = JSON.parse(provinces);
+
+      //console.log(provinceArray);
+      if (provinceArray.length < 1) {
+        provinceSelector.setAttribute("disabled", "disabled");
+      } else {
+        provinceSelector.removeAttribute("disabled");
+      }
+
+      provinceSelector.innerHTML = "";
+      var options = "";
+      for (var i = 0; i < provinceArray.length; i++) {
+        options +=
+          '<option value="' +
+          provinceArray[i][0] +
+          '">' +
+          provinceArray[i][0] +
+          "</option>";
+      }
+
+      provinceSelector.innerHTML = options;
+    });
+}
+
+if (document.getElementById("forgotPassword") != null) {
+  document
+    .getElementById("forgotPassword")
+    .addEventListener("click", function (e) {
+      const element = document.querySelector("#forgot_password_form");
+      if (element.classList.contains("d-none")) {
+        element.classList.remove("d-none");
+        element.classList.add("d-block");
+      }
+    });
+}
